4187e1106c8e015b7fa3a336bf6dfdda8d542b5a
ulbertAO <sp595993@gmail.com>
2023-10-08 17:09:59 +0530

    launchDarkly integration with some UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as LDClient from 'launchdarkly-js-client-sdk';
+import { AppConfigService } from './core/services/app-config.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'launch_darkly_demo';
+  username: string;
+  isSubmitted: boolean;
+  msg: string;
+
+  constructor(private config: AppConfigService) {
+  }
+  submit() {
+    this.isSubmitted = true;
+    if (this.username) {
+      this.launchDarkly()
+    }
+  }
+
+  launchDarkly() {
+    const launchDarklyconfig: any = this.config.config?.launchDarkly;
+    const context = {
+      key: this.username
+    };
+    const ldClient = LDClient.initialize(launchDarklyconfig.id, context);
+
+    ldClient.waitForInitialization()
+      .then(() => {
+        let grantAccess = ldClient.variation("IsPilotUser");
+        // console.log('ready: ', grantAccess);
+        this.access(grantAccess)
+
+
+        ldClient.on("change", (value: any) => {
+          if (value["IsPilotUser"]) {
+            grantAccess = value["IsPilotUser"].current;
+            // console.log("change", grantAccess);
+            this.access(grantAccess)
+          }
+        });
+        // setTimeout(() => {
+        //   window.location.reload()
+        // }, 2000);
+      })
+
+  }
+  access(grantAccess:boolean){
+    if (grantAccess) {
+      this.msg = "Welcome";
+    } else {
+      this.msg = "You are not authorised to access this page.";
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { APP_INITIALIZER } from '@angular/core'
 import { AppConfigService } from './core/services/app-config.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     {
